79787ce85f8de68bb8b51f681cc2510d8b0679ad
Indrajeet Sarkar <sarkarraj1978@gmail.com>
2024-02-21 21:37:41 +0530

    add : created the contract

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,31 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 describe("Dappazon", () => {
+  let dappazon;
+  let deployer, buyer;
 
-})
+  beforeEach(async () => {
+    //setup accounts
+    [deployer, buyer] = await ethers.getSigners();
+
+    //deploying the contract
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    dappazon = await Dappazon.deploy();
+  });
+
+  describe("Deployemnt", async () => {
+    it("Sets the Owner", async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address);
+    });
+    //
+    //Initial Testing
+    // it("has a name", async () => {
+    //   const name = await dappazon.name();
+    //   expect(name).to.equal("Dappazon");
+    // });
+  });
+});
