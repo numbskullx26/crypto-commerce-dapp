389ca387790ecb8d68ee16b17d7b5c2b1b2d8247
Indrajeet Sarkar <sarkarraj1978@gmail.com>
2024-02-21 22:57:04 +0530

    add : contract : list function

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -4,8 +4,46 @@ pragma solidity ^0.8.9;
 contract Dappazon {
     address public owner;
 
+    struct Item{
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    mapping(uint256 => Item) public items;
+
+    event List(string name, uint256 cost, uint256 quantity);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
     constructor() {
-    
+        
         owner = msg.sender;
+    }
+
+    //Listing Products
+    function list(uint256 _id,string memory _name, string memory _category, string memory _image,uint256 _cost,uint256 _rating,uint256 _stock) public onlyOwner{
+        
+        //Create Item Struct 
+        Item memory item = Item(_id,
+        _name,
+        _category,
+        _image,
+        _cost,
+        _rating,
+        _stock);
+
+
+        //Save Item Struct to the Blockchain
+        items[_id] = item;
+        emit List(_name,_cost,_stock);
+
     }
 }
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -4,6 +4,17 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
+//Global Constants for listing an item...
+
+const ID = 1;
+const NAME = "Shoes";
+const CATEGORY = "Clothing";
+const IMAGE =
+  "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg";
+const COST = tokens(1);
+const RATING = 4;
+const STOCK = 5;
+
 describe("Dappazon", () => {
   let dappazon;
   let deployer, buyer;
@@ -17,7 +28,7 @@ describe("Dappazon", () => {
     dappazon = await Dappazon.deploy();
   });
 
-  describe("Deployemnt", async () => {
+  describe("Deployment", async () => {
     it("Sets the Owner", async () => {
       expect(await dappazon.owner()).to.equal(deployer.address);
     });
@@ -27,5 +38,33 @@ describe("Dappazon", () => {
     //   const name = await dappazon.name();
     //   expect(name).to.equal("Dappazon");
     // });
+  });
+
+  describe("Listing", async () => {
+    let transaction;
+
+    beforeEach(async () => {
+      transaction = await dappazon
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+
+      await transaction.wait();
+    });
+
+    it("Returns item attributes", async () => {
+      const item = await dappazon.items(ID);
+
+      expect(item.id).to.equal(ID);
+      expect(item.name).to.equal(NAME);
+      expect(item.category).to.equal(CATEGORY);
+      expect(item.image).to.equal(IMAGE);
+      expect(item.cost).to.equal(COST);
+      expect(item.rating).to.equal(RATING);
+      expect(item.stock).to.equal(STOCK);
+
+      it("Emits List Event", async () => {
+        expect(transaction).to.emit(dappazon, "List");
+      });
+    });
   });
 });
