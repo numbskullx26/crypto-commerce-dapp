ec37fea7e85ad572ddc631ef65f6ae674ef66d15
Indrajeet Sarkar <sarkarraj1978@gmail.com>
2024-02-23 22:26:12 +0530

    add : metamask connection and navbar UI

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,45 @@
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 
 const Navigation = ({ account, setAccount }) => {
+  const connectHandler = async () => {
+    //used to request the user's account address from metamask
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = await ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
+  };
+  return (
+    <nav>
+      <div className="nav__brand">
+        <h1>Dappazon</h1>
+      </div>
 
-    return (
-        <nav>
+      <input type="text" className="nav__search" />
 
-        </nav>
-    );
-}
+      {account ? (
+        <button className="nav__connect" type="button">
+          {account.slice(0, 6) + "..." + account.slice(38, 42)}
+        </button>
+      ) : (
+        <button className="nav__connect" type="button" onClick={connectHandler}>
+          Connect
+        </button>
+      )}
+
+      <ul className="nav__links">
+        <li>
+          <a href="#Clothing & Jewelry">Clothing & Jewelry</a>
+        </li>
+        <li>
+          <a href="#Electronics & Gadgets">Electronics & Gadgets</a>
+        </li>
+        <li>
+          <a href="#Toys & Gaming">Toys & Gaming</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navigation;
